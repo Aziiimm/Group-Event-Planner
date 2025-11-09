ca97c2bb5169e5f254ce743371f755a3fe132db4
Shahed Ahmed <ShahedAhmedx1@gmail.com>
2025-11-09 18:01:06 -0500

    Backend Authentication Infrastructure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
+import { SupabaseModule } from './supabase/supabase.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    SupabaseModule,
+    AuthModule,
     HealthModule,
   ],
   controllers: [AppController],
