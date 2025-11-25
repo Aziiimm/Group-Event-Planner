82d0a9b6f3dd73f69b7edf715acd6a4b11c48bea
rahbinrackin <131881678+rahbinrackin@users.noreply.github.com>
2025-11-24 20:15:27 -0500

    Fixed /health endpoint test returning status 200. (#16)
    * Frontend: npm test runs a simple “renders HomeScreen” test
    
    * Fixed health-endpoint test

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -3,11 +3,14 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { config } from 'dotenv';
+import { join } from 'path';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
+    config({ path: join(__dirname, '..', '.env.local') });
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
